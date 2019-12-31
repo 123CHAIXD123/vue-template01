541f8a0131b1d9f32d310950087c5540206da9be
柴向东 <xd.chai@maxtekitsm.com>
2019-12-31 18:57:06 +0800

    ceshi

--- src/views/user/list.vue
+++ src/views/user/list.vue
@@ -4,12 +4,35 @@
       <el-button type="primary">Primary Button</el-button>
       <base-test></base-test>
       <base-input></base-input>
+      <ul v-for="(value, index, key) in list" :key="key">
+        {{ value }}
+        <li v-for="(values, indexs, keys) in value" :key="keys">
+          {{ values }}
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: {
+        '哦哦1': {
+          'a1': '玉玉1',
+          'a2': '玉玉2',
+          'a3': '玉玉3',
+          'a4': '玉玉4'
+        },
+        '哦哦2': {
+          'a1': '玉玉1',
+          'a2': '玉玉2',
+          'a3': '玉玉3',
+          'a4': '玉玉4'
+        }
+      }
+    }
+  }
 }
 </script>
 
